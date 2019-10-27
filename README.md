f543b036d4dbce5fbef905bfa96c71ca7b48a228
alexis <dondonalexis@hotmail.fr>
2019-10-27 10:47:26 +0100

    "init"

---
+++ README.md
@@ -0,0 +1,4 @@
+# JIB Example
+An example project that uses JIB to create Docker containers for Spring Boot.
+
+Check out my [Medium post](https://medium.com/@snewman/project-jib-quickstart-using-spring-boot-c7201166e36d) for more details.
---
+++ src/main/java/com/example/springjib/SpringJibApplication.java
@@ -0,0 +1,12 @@
+package com.example.springjib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJibApplication.class, args);
+	}
+}
