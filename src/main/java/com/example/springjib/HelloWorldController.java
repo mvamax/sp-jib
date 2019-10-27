ff88989336b0c0de72e1babd6b134e7d6f42e5cb
alexis <dondonalexis@hotmail.fr>
2019-10-27 18:29:13 +0100

    "update"

--- src/main/java/com/example/springjib/HelloWorldController.java
+++ src/main/java/com/example/springjib/HelloWorldController.java
@@ -2,13 +2,20 @@ package com.example.springjib;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.net.InetAddress;  
+import java.net.UnknownHostException;  
 @RestController
 public class HelloWorldController {
 
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, World!";
+        try {  
+            InetAddress id = InetAddress.getLocalHost();  
+            return "Hello, World v2 on "+id.getHostName();
+          } catch (UnknownHostException e) {  
+            return "Hello, World on undefined";
+          }  
     }
 
 }
